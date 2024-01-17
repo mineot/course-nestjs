d3f838d0580f91ab6ed3232d6adecfd15eb0532e
Mineot <mineot@gmail.com>
2024-01-17 18:47:05 -0300

    master - how enable cors and configura 2x

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { LogInterceptor } from './core/interceptors/log/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // TODO to more details see de doc on nest github, find by cors doc
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LogInterceptor()); // para que o interceptor esteja disponivel a todos os controllers
