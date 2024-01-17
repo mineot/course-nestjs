5ab589b299fc5844e6ad5cfa3de78ba2a4db076c
Mineot <mineot@gmail.com>
2024-01-17 18:46:28 -0300

    master - how enable cors and configura

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { LogInterceptor } from './core/interceptors/log/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LogInterceptor()); // para que o interceptor esteja disponivel a todos os controllers
   await app.listen(3000);
